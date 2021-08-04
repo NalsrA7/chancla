4f217e85b5e5757c9ef1511f748f7e50d37873a3
Arslan Ahmad <arslanshero@gmail.com>
2021-08-04 14:25:10 +0700

    moved dict to json file

--- foodrec.py
+++ foodrec.py
@@ -5,40 +5,14 @@
 ### Work In Progress ###
 
 import random
+import time
+import json
 
-foodie = {'breakfast':
-            {'hot':
-                {'heavy':['pancakes','avocado on toast','eggs on toast','french toast'],
-                'light':['egg wrap','poached eggs','egg quiche']},
-            'cold':
-                {'heavy':['nutella crepes','avocado on toast','smoothie bowl with fruits'],
-                'light':['cereal','PB&J','smoothie bowl']}},
-        'lunch':
-            {'hot':
-                {'heavy':['nasi goreng','lodeh lodeh','Big Mac'],
-                'light':['mcdonalds fillet-o-fish','chicken burrito','grilled cheese sandwich']},
-            'cold':
-                {'heavy':['chicken salad','big sushi roll','pesto pasta'],
-                'light':['fruit salad','tuna sandwich','smoked salmon on a bagel']}},
-        'dinner':
-            {'hot':
-                {'heavy':['beef steak','baked ziti pasta','cheese pizza','two indomie packets'],
-                'light':['rice and eggs','salmon and asparagus','one indomie packet']},
-            'cold':
-                {'heavy':['japanese soba','caesar salad'],
-                'light':['feta cheese pasta','cereal']}},
-        'dessert':
-            {'hot':
-                {'heavy':['pancakes','lava mug cake'],
-                'light':['gulab jamun','jalebi']},
-            'cold':
-                {'heavy':['choco pudding','chocolate cake'],
-                'light':['ice cream','vanilla panna cotta']}}}
-
-#food_rec = ''
+# Initializing food dictionary
+with open('foodie_dict.json','r') as json_file:
+    foodie = json.load(json_file)
 
 print('Welcome to the food recommender bot')
-
 
 while True:
 
@@ -53,7 +27,7 @@ while True:
 
 while True:
 
-    temp_of_meal = input('Now please input wether you would like hot or cold food! [hot/cold]')
+    temp_of_meal = input('Now please input wether you would like hot or cold food! [hot/cold]:\n')
     
     if temp_of_meal in foodie[time_of_meal]:
         print(f'You chose {temp_of_meal}!')
@@ -63,12 +37,84 @@ while True:
 
 while True:
 
-    size_of_meal = input('Finally, please input wether you would like heavy or light food! [heavy/light]')
+    size_of_meal = input('Finally, please input wether you would like heavy or light food! [heavy/light]:\n')
 
     if size_of_meal in foodie[time_of_meal][temp_of_meal]:
         print(f'You chose {size_of_meal}!')
-        food_rec = random.choice(foodie[time_of_meal][temp_of_meal][size_of_meal])
-        print(f'Hooray! You should eat {food_rec}!')
         break
     else:
         print('Sorry! you can only input either heavy or light. Please try again')
+
+food_rec = random.choice(foodie[time_of_meal][temp_of_meal][size_of_meal])
+print(f'Hooray! You should eat {food_rec}!')
+
+
+### THE FOLLOWING CODE IS FOR TAKING IN INPUT FOR THE FOOD DICTIONARY ###
+
+yes_choices = ['y','Y','yes','Yes','YES']
+no_choices = ['n','N','no','No','NO']
+
+time.sleep(4)
+print('Whould you like to recommend a food to be added to the list?')
+
+while True:
+    
+    choice_rec = input('[type Y for yes and N for no]')
+
+
+    if choice_rec in yes_choices:
+        
+        print('Thank you for choosing to expand our list of food')
+        time.sleep(2)
+
+        while True:
+
+            time_of_meal = input('Which time of meal would you like to recommend? [breakfast/lunch/dinner/dessert]:\n')
+    
+            if time_of_meal in foodie:
+                print(f'You chose {time_of_meal}!')
+                break
+            else:
+                print('Sorry! you can only input either breakfast, lunch, dinner, or dessert. Please try again')
+
+        while True:
+
+            temp_of_meal = input('Would you like to recommend hot or cold food! [hot/cold]:\n')
+    
+            if temp_of_meal in foodie[time_of_meal]:
+                print(f'You chose {temp_of_meal}!')
+                break
+            else:
+                print('Sorry! you can only input either hot or cold. Please try again')
+
+        while True:
+
+            size_of_meal = input('Will you recommend heavy or light food! [heavy/light]:\n')
+
+            if size_of_meal in foodie[time_of_meal][temp_of_meal]:
+                print(f'You chose {size_of_meal}!')
+                break
+            else:
+                print('Sorry! you can only input either heavy or light. Please try again')
+
+        new_food = input('Finally, please type what food would you like to recommend.\n')
+        
+        foodie[time_of_meal][temp_of_meal][size_of_meal].append(str(new_food))
+        with open('foodie_dict.json','w') as json_file:
+            json.dump(foodie,json_file,indent=4)
+        
+        time.sleep(2)
+        print(f'Thank you! {new_food} has been added to the list of foods :)')
+        break
+
+
+    elif choice_rec in no_choices:
+        break
+
+
+    else:
+        print('Sorry! You can only type Y for YES and N for NO.')
+
+### STILL NEED TO ADD CODE TO APPEND new_food TO THE DICTIONARY
+time.sleep(2)
+print('Thank you for using our food recommendation')
