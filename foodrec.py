a17d3ec7ddc0388f85a10aa537e46738ba62eb51
Arslan Ahmad <arslanshero@gmail.com>
2021-08-08 12:16:50 +0700

    completed food contribution feature
    Co-authored-by: whyfai <whyfai@users.noreply.github.com>

--- foodrec.py
+++ foodrec.py
@@ -8,7 +8,6 @@ import random
 import time
 import json
 import discord
-from discord.embeds import Embed
 from discord.ext import commands
 import asyncio
 
@@ -81,6 +80,8 @@ class SizeOfMeal(discord.ui.View):
         await interaction.response.edit_message(content=None,embed=embed,view=None)
         await asyncio.sleep(7)
         await message.reply(f'Hooray! Our expert AI recommends that you eat {food_rec}!')
+        await asyncio.sleep(4)
+        await message.reply("Do you want to recommend a food to be added to our database?", view=Confirmation())
         
     @discord.ui.button(style=discord.ButtonStyle.primary, label="Light")
     async def light(self, button, interaction):
@@ -96,7 +97,141 @@ class SizeOfMeal(discord.ui.View):
         await interaction.response.edit_message(content=None,embed=embed,view=None)
         await asyncio.sleep(7)
         await message.reply(f'Hooray! Our expert AI recommends that you eat {food_rec}!')
+        await asyncio.sleep(4)
+        await message.reply("Do you want to recommend a food to be added to our database?", view=Confirmation())
+
        
+       
+### THE FOLLOWING CODE IS FOR TAKING IN USER INPUT FOR THE FOOD DICTIONARY ###
+
+# buttons to ask USERS if they would like to contribute food to the dictionary.
+class Confirmation(discord.ui.View):
+    
+    @discord.ui.button(style=discord.ButtonStyle.success, label="Yes", emoji="<:pepehappy:873771479907577906>", row=0)
+    async def yes(self, button, interaction): 
+        response = 'Yes' 
+        await interaction.response.edit_message(content=f"Thank you for saying {response}!\nOk. Now please tell us what time of meal best fits the food you would like to contribute!", view=TimeOfMeal_Contrib())
+    
+    @discord.ui.button(style=discord.ButtonStyle.danger, label="No", emoji="<a:pepecry:873771306993197157>", row=0)
+    async def no(self, button, interaction):
+        response = "No"
+        await interaction.response.edit_message(content=f"You picked {response}!\nThat is very sad <a:pepecry:873771306993197157>", view=Confirmation_Disabled())
+    
+    @discord.ui.button(style=discord.ButtonStyle.secondary, label="View Current Foods", emoji="📖",row=1)
+    async def viewallfood(self, button, interaction):
+        all_foods = []
+        for x in foodie.values():
+            all_foods.append('\n\n')
+            for y in x.values():
+                for z in y.values():
+                    for f in z:
+                        all_foods.append(f+", ")
+                    
+        view_all = ''.join(all_foods)
+        await interaction.response.send_message(content="Current List of Foods\n""```"+view_all+"```")
+        
+class Confirmation_Disabled(discord.ui.View):
+    @discord.ui.button(style=discord.ButtonStyle.success, label="Yes", emoji="<:pepehappy:873771479907577906>", row=0, disabled=True)
+    async def yes_disabled(self, button, interaction):
+        pass
+    
+    @discord.ui.button(style=discord.ButtonStyle.danger, label="No", emoji="<a:pepecry:873771306993197157>", row=0, disabled=True)
+    async def no_disabled(self, button, interaction):
+        pass
+    
+    @discord.ui.button(style=discord.ButtonStyle.secondary, label="View Current Foods", emoji="📖",row=1, disabled=True)
+    async def viewallfood_disabled(self, button, interaction):
+        pass    
+    
+       
+
+# buttons for taking in input for time of meal for USERS food contribution
+class TimeOfMeal_Contrib(discord.ui.View):
+   
+    @discord.ui.button(style=discord.ButtonStyle.primary, label="Breakfast")
+    async def breakfast(self, button, interaction):
+        global time_of_meal 
+        time_of_meal = "breakfast"
+        await interaction.response.edit_message(content=f"You picked {time_of_meal}!\nNow please input whether the food you are recommending is hot or cold!", view=TempOfMeal_Contrib())
+    
+    @discord.ui.button(style=discord.ButtonStyle.primary, label="Lunch")
+    async def lunch(self, button, interaction):
+        global time_of_meal 
+        time_of_meal = "lunch"
+        await interaction.response.edit_message(content=f"You picked {time_of_meal}!\nNow please input whether the food you are recommending is hot or cold!", view=TempOfMeal_Contrib())
+
+    @discord.ui.button(style=discord.ButtonStyle.primary, label="Dinner")
+    async def dinner(self, button, interaction):
+        global time_of_meal 
+        time_of_meal = "dinner"
+        await interaction.response.edit_message(content=f"You picked {time_of_meal}!\nNow please input whether the food you are recommending is hot or cold!", view=TempOfMeal_Contrib())
+        
+    @discord.ui.button(style=discord.ButtonStyle.primary, label="Dessert")
+    async def dessert(self, button, interaction):
+        global time_of_meal 
+        time_of_meal = "dessert"
+        await interaction.response.edit_message(content=f"You picked {time_of_meal}!\nNow please input whether the food you are recommending is hot or cold!", view=TempOfMeal_Contrib())
+
+
+# buttons for taking in input for temperature of meal for USERS food contribution
+class TempOfMeal_Contrib(discord.ui.View):
+    
+    @discord.ui.button(style=discord.ButtonStyle.danger, label="Hot", emoji="🔥")
+    async def hot(self, button, interaction):
+        global temp_of_meal 
+        temp_of_meal = "hot"
+        await interaction.response.edit_message(content=f"You picked {temp_of_meal}!\nFinally, please input whether your food is heavy or light food!", view=SizeOfMeal_Contrib())
+    
+    @discord.ui.button(style=discord.ButtonStyle.primary, label="Cold", emoji="❄")
+    async def cold(self, button, interaction):
+        global temp_of_meal
+        temp_of_meal = "cold"
+        await interaction.response.edit_message(content=f"You picked {temp_of_meal}!\nFinally, please input whether your food is heavy or light food!", view=SizeOfMeal_Contrib())
+
+
+# buttons for taking in input for size of meal for USERS food contribution
+class SizeOfMeal_Contrib(discord.ui.View):
+    
+    @discord.ui.button(style=discord.ButtonStyle.primary, label="Heavy")
+    async def heavy(self, button, interaction):
+        
+        global size_of_meal
+        size_of_meal = "heavy"
+        food_rec = random.choice(foodie[time_of_meal][temp_of_meal][size_of_meal])
+        await interaction.message.reply(f"YYou chose to input a {temp_of_meal} meal that is {size_of_meal} and is eaten for {time_of_meal}.\nNow please type the food you would like to contribute and hit send\n(e.g. chicken nuggets)")
+        await Contribution(interaction)
+        
+    @discord.ui.button(style=discord.ButtonStyle.primary, label="Light")
+    async def light(self, button, interaction):
+        
+        global size_of_meal
+        size_of_meal = "light"
+        food_rec = random.choice(foodie[time_of_meal][temp_of_meal][size_of_meal])
+        await interaction.message.reply(f"You chose to input a {temp_of_meal} meal that is {size_of_meal} and is eaten for {time_of_meal}.\nNow please type the food you would like to contribute and hit send\n(e.g. chicken nuggets)")
+        await Contribution(interaction)
+
+
+async def Contribution(interaction):
+    
+    def check(msg):
+        return msg.author == interaction.user and msg.channel == interaction.channel
+    try:
+        msg = await client.wait_for("message", check=check, timeout=30) # 30 seconds to reply
+    except asyncio.TimeoutError:
+        await interaction.response.send_message(content="Sorry, you didn't reply in time!\nThank you for using our food recommendation bot\nCreated by @Nalsra7 [github] and @whyfai [github & discord]")
+     
+    with open("foodie_contrib.json","r") as f:
+        foodie_contrib = json.load(f) 
+            
+    foodie_contrib[time_of_meal][temp_of_meal][size_of_meal].append(msg.content)
+        
+    with open('foodie_contrib.json','w') as json_file:
+        json.dump(foodie_contrib,json_file,indent=4)
+        
+    await msg.reply("Thank you for making our food recommendation bot even better!\nThe developers will verify your contribution ASAP!\nCreated by @Nalsra7 [github] and @whyfai [github & discord]")       
+
+
+### COMMANDS GO BELOW ###      
 # to announce bot being operational
 @client.event
 async def on_ready():
@@ -107,89 +242,9 @@ async def on_ready():
 async def hungry(ctx):
     global message
     message = await ctx.send("Which time of meal would you like a recommendation for?", view=TimeOfMeal())
-  
+
+
+#if __name__ in '__main___':
+#    print('el potata el seniorita, chimpanzee sobrar')
     
-client.run("ODczMzk2MjMyMjU5NzY0MjU1.YQ3zoA.Gp-_FpHEY0FDbdbSzRyF6G_Kv5g")
-
-
-
-### THE FOLLOWING CODE IS FOR TAKING IN INPUT FOR THE FOOD DICTIONARY ###
-
-yes_choices = ['y','Y','yes','Yes','YES']
-no_choices = ['n','N','no','No','NO']
-food_opt = ['food_options','food options']
-list_food = []
-
-time.sleep(3)
-print('Whould you like to recommend a food to be added to the list?')
-
-while True:
-    
-    choice_rec = input('[type Y for yes and N for no]')
-
-
-    if choice_rec in yes_choices:
-        
-        print('Thank you for choosing to expand our list of food')
-        time.sleep(2)
-
-        while True:
-
-            print('to see the current options of food, type [food_options]')
-            time_of_meal = input('Which time of meal would you like to recommend? [breakfast/lunch/dinner/dessert]:\n').lower()
-    
-            if time_of_meal in foodie:
-                print(f'You chose {time_of_meal}!')
-                break
-            
-            elif time_of_meal in food_opt:
-                for x in foodie.values():
-                    for y in x.values():
-                        for z in y.values():
-                            print(z)
-            
-            else:
-                print('Sorry! you can only input either breakfast, lunch, dinner, or dessert. Please try again')
-
-        while True:
-
-            temp_of_meal = input('Would you like to recommend hot or cold food! [hot/cold]:\n').lower()
-    
-            if temp_of_meal in foodie[time_of_meal]:
-                print(f'You chose {temp_of_meal}!')
-                break
-            else:
-                print('Sorry! you can only input either hot or cold. Please try again')
-
-        while True:
-
-            size_of_meal = input('Will you recommend heavy or light food! [heavy/light]:\n').lower()
-
-            if size_of_meal in foodie[time_of_meal][temp_of_meal]:
-                print(f'You chose {size_of_meal}!')
-                break
-            else:
-                print('Sorry! you can only input either heavy or light. Please try again')
-
-
-        new_food = input('Finally, please type what food would you like to recommend.\n')
-        
-        foodie[time_of_meal][temp_of_meal][size_of_meal].append(str(new_food))
-        
-        with open('foodie_dict.json','w') as json_file:
-            json.dump(foodie,json_file,indent=4)
-        
-        time.sleep(2)
-        print(f'Thank you! {new_food} has been added to the list of foods :)')
-        break
-
-
-    elif choice_rec in no_choices:
-        break
-
-
-    else:
-        print('Sorry! You can only type Y for YES and N for NO.')
-
-time.sleep(2)
-print('Thank you for using our food recommendation!')
+client.run("ODczMzk2MjMyMjU5NzY0MjU1.YQ3zoA.Gp-_FpHEY0FDbdbSzRyF6G_Kv5g") 
